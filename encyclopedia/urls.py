e5c08b5e962f8310e63f71af8a73058e8c8d9753
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2020-10-09 15:54:47 -0700

    Add wiki to some routes

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = "encyclopedia"
 
 urlpatterns = [
+    path("wiki", views.index, name="wiki"),
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
     path("new-page", views.new_page, name="new-page"),
     path("random", views.random_page, name="random"),
-    path("<str:title>", views.entry, name="entry"),
-    path("<str:title>/edit", views.edit, name="edit")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>/edit", views.edit, name="edit")
 ]
